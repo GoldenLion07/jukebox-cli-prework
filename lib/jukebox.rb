4804a2c1c75e490988fdcbc6680abd2705c72485
Scott M <gmat191@gmail.com>
2018-10-17 04:00:35 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,6 +10,8 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+
+
 def help
   help = <<-HELP
 I accept the following commands:
@@ -22,5 +24,52 @@ HELP
   puts help
 end
 
-help
+help 
+
+def list(songs) 
+  songs.each_with_index { |item, index|
+    puts "#{index+1}. #{item}" }
+end 
+
+list(songs)
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_response = gets.downcase.chomp 
+  
+  if (1..9).to_a.include?(user_response.to_i)
+    puts "Playing #{songs[user_response.to_i - 1]}"
+    elsif songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else 
+    puts "Invalid input, please try again"
+  end 
+end 
+
+def exit_jukebox
+  puts "Goodbye"
+end 
+
+def run(songs)
+  #help
+  command = "" 
+  while command  
+  puts "Please enter a command:"
+  command = gets.downcase.strip  
+  case command 
+    when 'list'
+      list(songs)
+      when 'play'
+        list(songs)
+        play(songs)
+      when 'help'
+        help 
+      when 'exit'
+        exit_jukebox
+        break 
+      else 
+        help 
+      end 
+    end 
+  end 
   
